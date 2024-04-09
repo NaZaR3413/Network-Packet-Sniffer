3a0aec6985a8b6f3541118fd2d0cc1de42eb3d6d
Nilay Patel <nilayno1@gmail.com>
2024-04-08 19:42:41 -0700

    whois search

--- sniffer.py
+++ sniffer.py
@@ -1,5 +1,6 @@
 from scapy.all import sniff, show_interfaces, IP
 from dns import reverse_dns
+import whois
 
 def packet_callback(packet):
     if packet.haslayer(IP):
@@ -10,14 +11,19 @@ def packet_callback(packet):
         # Ignore mDNS traffic
         if packet[IP].dst == "224.0.0.251" or packet[IP].dst == "ff02::fb":
             return
-    
         
         # attampt to grab source/destination names
         src_name = reverse_dns(src_ip)
         dst_name = reverse_dns(dest_ip)
         
-        print("\npacket summary: " + packet.summary())
+        print("\n\n\npacket summary: " + packet.summary())
         print(f"Packet: {src_ip} ({src_name}) -> {dest_ip} ({dst_name})")
+        
+        # use whois if more information wanted
+        #src_who = whois.whois(src_ip) # assign whois search on source
+        #dst_who = whois.whois(dst_ip) # assign whois search on destination
+        #print(src_who) 
+        #print(dst_who) 
         
 
 def main():
@@ -27,9 +33,9 @@ def main():
     #interface to search through 
     iface_name = "Intel(R) Wireless-AC 9462"  
 
-    # Start sniffing on the specified interface
+    # adjust the range according to how many packets you want to sniff
     for i in range(20):
-        sniff(iface=iface_name, prn=packet_callback, count=1)
+        sniff(iface=iface_name, prn=packet_callback, count=1) # Start sniffing on the specified interface
 
 if __name__ == "__main__":
     main()
